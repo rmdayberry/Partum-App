4a45226be7dbc4ede4e5c73bd0873b1fe39001fc
rmdayberry <dayberryrm@gmail.com>
2024-11-21 10:58:21 -0500

    project setup with frontend and backend structure
    Setup React Native and basic Express server

---
+++ .history/server/index_20241121105145.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
