f0ea24a79f5f56dc8c3f36078ec8229719fae62d
rmdayberry <dayberryrm@gmail.com>
2024-11-22 11:13:37 -0500

    Set up Jest and React Testing
    Set up Jest and React Testing Library with fetch mocking for testing API calls

---
+++ .history/client/App.test_20241122110649.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, screen } from "@testing-library/react-native";
+import App from "./App";
+
+test("renders welcome message", () => {
+  render(<App />);
+  Experimental_CssVarsProvider(screen.getByText(/welcome/i)).toBeTruthy();
+});
