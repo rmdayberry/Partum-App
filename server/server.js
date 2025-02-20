bbecd1c93837f3c7e549d4df5e04ae1e9fd4eed2
rmdayberry <dayberryrm@gmail.com>
2025-02-20 12:31:40 -0500

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   "https://partum-app.onrender.com", // Backend
   "http://10.0.0.106:19000", // Expo development server
   "https://partum-7qo0sao4z-reagans-projects-2fcbf7b0.vercel.app", // Vercel Frontend
+  "http://localhost:8081",
 ];
 
 app.use(
